6a630da6a8ad00880716f526c8ab3e48fc997a6a
Nguyễn Hoàng Dương <you_create@protonmail.com>
2018-03-21 15:05:55 +0700

    Indent properly PLEASE STUPID AF EXTENSION

--- src/timmy.pas
+++ src/timmy.pas
@@ -3,16 +3,16 @@ Unit timmy;
 Interface
 Uses Classes;
 Type
-	TTimmy = Object
-			     Enabled: Boolean;
-				 NotUnderstandReply: String;
-				 Initialized: Boolean;
-				 MetaData: Array of Array of String;
-				 Function Init:Integer;
-				 Function Add(QKeywords, Replies, Pararms: Array of String):Integer;
-				 Function Remove(QKeywords: Array of String):Integer;
-				 Function Answer(TQuestion: String):String;
-			 End;
+    TTimmy = Object
+                 Enabled: Boolean;
+                 NotUnderstandReply: String;
+                 Initialized: Boolean;
+                 MetaData: Array of Array of String;
+                 Function Init:Integer;
+                 Function Add(QKeywords, Replies, Pararms: Array of String):Integer;
+                 Function Remove(QKeywords: Array of String):Integer;
+                 Function Answer(TQuestion: String):String;
+             End;
 
 Implementation
 End.
