5d774a0ad39114c67719c98b9c37d0181851dcca
Nguyen Hoang Duong <you_create@protonmail.com>
2018-05-27 08:54:10 +0700

    Update license notice for Shell's logger

--- shell/logger/logger.pas
+++ shell/logger/logger.pas
@@ -3,18 +3,21 @@
 
     Copyright (C) 2018 42tm Team <fourtytwotm@gmail.com>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This file is part of Timmy Interactive Shell.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Timmy Interactive Shell is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation, either version 3
+    of the License, or (at your option) any later version.
+
+    Timmy Interactive Shell is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    along with Timmy Interactive Shell.
+    If not, see <http://www.gnu.org/licenses/>.
 }
 {$mode ObjFPC}
 Unit Logger;
