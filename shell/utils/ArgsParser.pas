b7b272b316292cbb1778831a0a01629e5308a0e2
Nguyen Hoang Duong <you_create@protonmail.com>
2018-05-31 19:29:25 +0700

    Add license notices for ArgsParser.pas

--- shell/utils/ArgsParser.pas
+++ shell/utils/ArgsParser.pas
@@ -1,3 +1,22 @@
+{
+    ArgsParser - Command line argument parser for Delphi
+
+    Original Work: Copyright (C) 2012 Shinya Okano
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom
+    the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+}
+{
+    UPDATES
+        31 May 2018: Change unit name to "ArgsParser" - Committed by 42tm Team
+}
 unit ArgsParser;
 
 {$IFDEF FPC}
