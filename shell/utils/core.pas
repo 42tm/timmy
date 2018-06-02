de514466101d87735219e9f5aa1a333529a8fbf6
Nguyen Hoang Duong <you_create@protonmail.com>
2018-06-02 17:15:47 +0700

    Implement init (Prototype)

--- shell/utils/core.pas
+++ shell/utils/core.pas
@@ -25,6 +25,8 @@ Unit Core;
 Interface
 
 Uses
+     Crt, SysUtils,
+     ArgsParser,
      Timmy_Debug in '../../variants/timmy_debug.pas',
      Logger in '../logger/logger.pas';
 Type
@@ -32,33 +34,143 @@ Type
                  Command: String;
                  Args: TStrArray;
                End;
+Const
+    TIMMYVERSION = '1.2.0';
 Var
-    TestSubject: TTimmy;    // Subject TTimmy instance
-    ShellLogger: TLogger;   // Logger for Timmy Interactive Shell
-    InputRec: TUserCmd;     // User input data record
-    Initiated: Boolean;     // State of initialization of the test subject
+    TestSubj: TTimmy;   // Subject TTimmy instance
+    ShellLogger: TLogger;  // Logger for Timmy Interactive Shell
+    InputRec: TUserCmd;    // User input data record
+    InstanceName: String;  // Name of the test subject instance
+    Initiated: Boolean;    // State of initialization of the test subject
+    CmdHistory: TStrArray;
 
+    // Arguments parsing mechanisms
+      ArgParser: TArgumentParser;
+      OutParse: TParseResult;
+
+Function BoolToStr(AnyBool: Boolean): String;
 Procedure ShellExec(ShellInput: String);
 Procedure Init;
 
 Implementation
 
-Procedure ShellExec(ShellInput: String);
-Var FlagSplit: TStrArray;
+Function BoolToStr(AnyBool: Boolean): String;
 Begin
-    FlagSplit := StrSplit(ShellInput, ' ', True);
+    If AnyBool then Exit('True');
+    Exit('False');
+End;
+
+Procedure ShellExec(ShellInput: String);
+Var
+    FlagSplit: TStrArray;  // Command split result
+Begin
+    If OutParse.HasArgument('esc-space')
+      then FlagSplit := StrSplit(ShellInput, ' ', True)
+      else FlagSplit := StrSplit(ShellInput, ' ', False);
     InputRec.Command := FlagSplit[0];
     InputRec.Args := Copy(FlagSplit, 1, High(FlagSplit));
-
+    Writeln;
     Case InputRec.Command of
+      'exit', 'quit': Begin Writeln; Halt; End;
+      'clear': ClrScr;
       'init': If not Initiated then Init
-                else ShellLogger.Log(TLogger.INFO, 'Instance already initiated.');
+                else ShellLogger.Log(TLogger.INFO, 'Instance already initiated', True);
+      'add': Begin
+
+             End;
+      Else Begin
+             ShellLogger.Log(TLogger.ERROR, 'Invalid command ''' + InputRec.Command + '''', True);
+             // Remove command from command history because it's invalid
+               SetLength(CmdHistory, Length(CmdHistory) - 1);
+           End;
     End;
 End;
 
 Procedure Init;
+Var
+    Input: String;
+    FlagPercent: Integer;
+    FlagDefaultRep: String;
+    FlagDpCheck: Boolean;
+    ValErrorCode, FlagSeverity: Integer;
 Begin
+    If Length(InputRec.Args) = 0
+      then Begin
+             // Get value for TTimmy.TPercent
+               ValErrorCode := 1;
+               While ValErrorCode <> 0
+                 do Begin
+                      TextColor(9); Write(InstanceName + '.TPercent');
+                      TextColor(White); Write(' = ');
+                      Readln(Input);
+                      If Input = ''
+                        then Exit
+                        else Val(Input, FlagPercent, ValErrorCode);
+                      If ValErrorCode <> 0
+                        then ShellLogger.Log(TLogger.WARNING, 'Invalid value for ' + InstanceName + '.TPercent', True);
+                    End;
+             // Get value for TTimmy.DefaultRep
+               TextColor(9); Write(InstanceName + '.NoUdstdRep');
+               TextColor(White); Write(' = ');
+               Readln(Input);
+               If Input = ''
+                 then ShellLogger.Log(TLogger.LIGHTWARNING, 'Default reply should not be empty', True);
+               FlagDefaultRep := Input;
+             // Get value for TTimmy.DupesCheck
+               Input := '';
+               While (Input <> 'true') and (Input <> 'false')
+                 do Begin
+                      TextColor(9); Write(InstanceName + '.DupesCheck');
+                      TextColor(White); Write(' = ');
+                      Readln(Input);
+                      If Input = '' then Exit;
+                      Input := LowerCase(Input);
+                      If (Input <> 'true') and (Input <> 'false')
+                        then ShellLogger.Log(TLogger.WARNING, 'Expected a boolean value (true|false)', True);
+                    End;
+               If Input = 'true'
+                 then FlagDpCheck := True
+                 else FlagDpCheck := False;
+           End
+    else Begin
+           If (Length(InputRec.Args) < 3)
+             then Begin
+                    ShellLogger.Log(TLogger.ERROR, 'init: Wrong number of arguments to init', True);
+                    Exit;
+                  End;
+           Val(InputRec.Args[0], FlagPercent, ValErrorCode);
+           If ValErrorCode <> 0
+             then Begin
+                    ShellLogger.Log(TLogger.ERROR, 'Invalid value for ' + InstanceName + '.TPercent', True);
+                    Exit;
+                  End;
+           FlagDefaultRep := StrJoin(Copy(InputRec.Args, 1, Length(InputRec.Args) - 2), ' ');
+           Input := LowerCase(InputRec.Args[High(InputRec.Args)]);
+           Case Input of
+             'true': FlagDpCheck := True;
+             'false': FlagDpCheck := False;
+             Else Begin
+                    ShellLogger.Log(TLogger.ERROR, 'Invalid value for ' + InstanceName + '.DupesCheck', True);
+                    Exit;
+                  End;
+           End;
+         End;
 
+    TestSubj.Init(FlagPercent, FlagDefaultRep, FlagDpCheck);
+    ShellLogger.Log(TLogger.INFO, 'Instance initiated');
+    ShellLogger.Log(TLogger.INFO, 'Expected: ' + InstanceName + '.TPercent = '
+                  + IntToStr(FlagPercent) + '; ' + InstanceName + '.NoUdstdRep = '
+                  + FlagDefaultRep + '; ' + InstanceName + '.DupesCheck = '
+                  + BoolToStr(FlagDpCheck));
+    FlagSeverity := TLogger.CORRECT;
+    If (TestSubj.TPercent <> FlagPercent) or (TestSubj.NoUdstdRep <> FlagDefaultRep)
+       or (TestSubj.DupesCheck <> FlagDpCheck)
+      then FlagSeverity := TLogger.ERROR;
+
+    ShellLogger.Log(FlagSeverity, 'Got: ' + InstanceName + '.TPercent = '
+                  + IntToStr(TestSubj.TPercent) + '; ' + InstanceName + '.NoUdstdRep = '
+                  + TestSubj.NoUdstdRep + '; ' + InstanceName + '.DupesCheck = '
+                  + BoolToStr(TestSubj.DupesCheck));
 End;
 
 End.
